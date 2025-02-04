0a67ede4228ca2d3db1e6b14c44bd3908a4dd1d8
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-04 17:58:06 +0100

    Render the app titles in two columns

--- .venv/main.py
+++ .venv/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -11,6 +12,28 @@ with col2:
     st.title("Szymon Stefański")
     content = """
     I'm Szymon, a 3rd-year Computer Science student at the Polish-Japanese Academy of Information Technology. 
-    I love learning new technologies and applying them to real-world problems.
+    I love learning new technologies and applying them to real-world problems.\n
+    📚 What I’m learning:\n
+    I'm focusing on enhancing my skills in:
+    
+    Learning Python and Bash, particularly for automation tasks such as streamlining workflows, managing files, 
+    and creating utility scripts.\n
+    Database management and optimization with Oracle SQL and PL/SQL.\n
+    Frontend development using HTML, CSS, and JavaScript.\n
+    Exploring Arduino for hobby projects and understanding hardware-software integration.\n
     """
     st.info(content)
+
+st.write("Example text to show")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv(".venv/data.csv", sep=";")
+
+with col3:
+    for index, row in df[:1].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[1:].iterrows():
+        st.header(row["title"])
