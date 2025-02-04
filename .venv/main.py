ef0d1a1000ca1b498d291e1da5b7f56c601cbd24
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-04 18:14:41 +0100

    Add descriptions, images and links

--- .venv/main.py
+++ .venv/main.py
@@ -26,14 +26,20 @@ with col2:
 
 st.write("Example text to show")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv(".venv/data.csv", sep=";")
 
 with col3:
     for index, row in df[:1].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(".venv/Images/" + row["image"])
+        st.write(f"[Github repository]({row['url']})")
 
 with col4:
     for index, row in df[1:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        #st.image(".venv/Images/" + row["image"])
+        st.write(f"[Github repository]({row['url']})")
