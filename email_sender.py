a746b6a86237952d85f16b2c90aee526d8e6af04
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-06 19:34:29 +0100

    Add feature to send emails from contact form

---
+++ email_sender.py
@@ -0,0 +1,17 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "python.email12025@gmail.com"
+    password = "gsse dvli ihmn lsco"
+
+    receiver = "szymonstefanski1@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
